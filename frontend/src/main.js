62f90300951e915be1e8dd6680c67605b9ee63ad
Erick Lourenco <lourenco.erick@gmail.com>
2024-09-26 16:51:36 +0000

    chore(frontend): Add Toast and router to the app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+import './style.css';
+import 'primeicons/primeicons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -7,5 +11,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(Toast)
 
 app.mount('#app')
