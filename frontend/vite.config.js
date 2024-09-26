fa00b10791fedbfe9c9bb40fcda520a87f23a704
Erick Lourenco <lourenco.erick@gmail.com>
2024-09-26 16:48:54 +0000

    chore(frontend): Add proxy config to localhost api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,17 @@ export default defineConfig({
   ],
   server : {
     port: 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) =>  path.replace(/^\/api/, ''),
+      }
+    },
+    fs: {
+      strict: false,
+    },
   },
   resolve: {
     alias: {
