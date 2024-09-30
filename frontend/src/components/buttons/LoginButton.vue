170332d2ed24ff1d695c94d27da8cfb6e8acb7ca
Erick Lourenco <lourenco.erick@gmail.com>
2024-09-30 21:31:51 +0000

    feat(frontend): Add Login and Logout buttons

---
+++ frontend/src/components/buttons/LoginButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore(); 
+// authStore.initAuth();
+
+const login = async () => {
+    await authStore.login();
+};
+
+</script>
+
+<template>
+    <button @click="login"
+        class="relative flex items-center px-4 py-2 rounded-lg hover:text-white transition-colors duration-300 hover:bg-red-500 bg-gray-700">
+        <i class="pi pi-user mr-1"></i>
+        <pre> LOGIN</pre>
+    </button>
+</template>
---
+++ frontend/src/components/buttons/LogoutButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const auth0 = useAuth0();
+
+const logout = () => {
+    auth0.logout();
+};
+</script>
+
+<template>
+    <button @click="logout"
+        class="relative flex items-center px-4 py-2 rounded-lg hover:text-white transition-colors duration-300 hover:bg-red-500 bg-gray-700">
+        <i class="pi pi-sign-out mr-1"></i>
+        <pre> LOGOUT</pre>
+    </button>
+</template>
